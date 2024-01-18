23f9d42b42c79b59e0c5b4a51d94b510a0625129
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2024-01-18 20:43:02 -0300

    2 evitar ir   window.location.href = inicio-sesion.html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,7 @@
     if (!userConfirmed) {
       setTimeout(function () {
     document.getElementById("popupModal").style.display = "block";
-     }, 30000);
+     }, 90000);
     } else {
        // Si el usuario está confirmado, oculta el popupModal
        document.getElementById("popupModal").style.display = "none";
